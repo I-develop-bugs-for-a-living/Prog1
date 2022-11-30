6e1f03a0b6499f2dd31540147ea1f2d589f98d75
Jan Buehne <64044789+I-develop-bugs-for-a-living@users.noreply.github.com>
2022-11-30 16:00:13 +0100

    All loops working

--- Assignment07/loops.c
+++ Assignment07/loops.c
@@ -54,47 +54,123 @@ void loops_c(int n) {
 
 void loops_d(int n) {
     // todo: implement
+    int limit = (n - 1) / 2;
     for (int i = 0; i < n; i++) {
-        for (int j = n - i; j < n; j++) {
-            printf("  ");
+        for (int j = 0; j < n; j++) {
+            if (i == j || i + j == n-1) {
+                if (i < limit) {
+                    printf("%d ", i);
+                } else {
+                printf("%d ", n-1-i);
+                }
+            } else {
+                printf("  ");
+            }
         }
-
-        printf("%d", i);
-
-        for (int k = 0; k < n - i; k++) {
-            printf(" ");
-        }
-
-        printf("%d \n", i);
+        printf("\n");
     }
 }
 
 // todo: implement helper functions for loops_e (recommended)
+/*
+
+    +--------+
+   /        /
+  /        /
+ /        /
++--------+
+
+*/
+
+void middle_part(int n) {
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || i == n-1) {
+            printf("/");
+        } else {
+            printf(" ");
+        }
+    }
+}
+
+void edges(int n) {
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || i == n-1) {
+            printf("+");
+        } else {
+            printf("-");
+        }
+    }
+}
 
 void loops_e(int n) {
     // todo: implement
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+            printf(" ");
+        }
+        if (i == 0 || i == n-1) {
+            edges(n*2);
+        } else {
+            middle_part(n*2);
+        }
+        printf("\n");
+    }
+
 }
 
 // todo: implement helper functions for loops_f (recommended)
 
+void middle_part_f(int n, bool new) {
+    static int num = 0;
+    if (new) {
+        num = 0;
+    }
+
+    for (int i = 0; i < n/2; i++) {
+        if (i == 0 || i == n/2) {
+            printf("/");
+        } else {
+            printf("%d ", num);
+            num++;
+            if (num > 9) {
+                num = 0;
+            }
+        }
+    }
+    printf("/");
+}
+
 void loops_f(int n) {
+    bool new = true;
     // todo: implement
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+            printf(" ");
+        }
+        if (i == 0 || i == n-1) {
+            edges(n*2);
+        } else {
+            middle_part_f(n*2, new);
+            new = false;
+        }
+        printf("\n");
+    }
 }
 
 int main(void) {
     // loops_a(5);
     // loops_b(5);
     // loops_c(5);
-    loops_d(5);
+    // loops_d(5);
     // loops_a(9);
     // loops_b(9);
     // loops_c(9);
     // loops_d(9);
-    loops_e(3);
-    loops_e(4);
-    loops_e(5);
-    loops_e(9);
-    loops_e(12);
+    //loops_e(3);
+    //loops_e(4);
+    //loops_e(5);
+    //loops_e(9);
+    //loops_e(12);
     loops_f(5);
     loops_f(9);
     return 0;
